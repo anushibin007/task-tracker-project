02ceb515e89c9b3008c3342d37d132b306caadb4
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2025-11-06 23:12:48 +0530

    ✨ Backend CORS config to allow requests from all origins

---
+++ backend/src/main/java/com/fastorial/todo/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.fastorial.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // Allow all endpoints
+						.allowedOrigins("*") // Allow all origins
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all common methods
+						.allowedHeaders("*"); // Allow all headers
+			}
+		};
+	}
+}
